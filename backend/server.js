2416fef68e26e4cfd8298820a7c32b4d3bc18662
hannahd96 <34348535+hannahd96@users.noreply.github.com>
2019-11-14 00:40:44 +0000

    #1 UI changes

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,17 @@ require('dotenv').config();
 const app = express();
 const port= process.env.PORT || 5000;
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("Mongo DB connection successful");
-})
-
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongo DB connection successful :)");
+})
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Logo from "./images/logo.png";
+import arrow from "./images/arrow-img.png"
 import './App.css';
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <h1>Welcome to my React App</h1>
-          <div className="test">
-            lorem ipsum lorem ipsum lorem ipsum
-          </div>
-      </header>
+     <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <span><img className="navbar-logo" alt="logo" src={Logo}/></span>
+          <span id="brand-text">Notes</span>
+            <button className="btn btn-outline-success" id="create-btn">Create</button>
+        </a>
+      </nav>
+      <div className="col-sm-3">
+        <div className="wrapper">
+            <nav id="sidebar">
+                <ul className="list-unstyled components">
+                  <li className="active">
+                    <a href="#">Groceries<img src={arrow} alt="arrow" className="arrow"></img></a>
+                  </li>
+                  <li>
+                      <a href="#">Todos<img src={arrow} alt="arrow" className="arrow"></img></a>
+                  </li>
+                  <li>
+                      <a href="#">Book Titles<img src={arrow} alt="arrow" className="arrow"></img></a>
+                  </li>
+                  <li>
+                      <a href="#">Conference Details<img src={arrow} alt="arrow" className="arrow"></img></a>
+                  </li>
+                  <li>
+                      <a href="#">Meeting Feedback<img src={arrow} alt="arrow" className="arrow"></img></a>
+                  </li>
+                </ul>
+            </nav>
+        </div>
+      </div>
     </div>
   );
 }
