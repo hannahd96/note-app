f7282d0b7a823f622b894e7df4f20777b27c25a0
hannahd96 <34348535+hannahd96@users.noreply.github.com>
2019-11-19 20:56:00 +0000

    full CRUD functionality

--- src/components/edit-note.component.js
+++ src/components/edit-note.component.js
@@ -1,10 +1,127 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class EditNotes extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeContent = this.onChangeContent.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state= {
+            username: '',
+            title: '',
+            content: '',
+            users: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/notes/' + this.props.match.params.id)
+            .then(response => {
+                this.setState({
+                    username: response.data.username,
+                    title: response.data.title,
+                    content: response.data.content
+                })
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+
+        axios.get('http://localhost:5000/users/')
+            .then(response => {
+                if(response.data.length > 0){
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                    })
+                }
+            })
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangeTitle(e){
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    onChangeContent(e){
+        this.setState({
+            content: e.target.value
+        });
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const note = {
+            username: this.state.username,
+            title: this.state.title,
+            content: this.state.content
+        }
+        
+        console.log(note);
+
+        axios.post('http://localhost:5000/notes/update/' + this.props.match.params.id, note)
+            .then(res => console.log(res.data));
+
+        window.location = '/';
+    }
+    
     render() {
         return (
             <div>
-                <p>YOU ARE ON THE NOTES EDIT Component</p>
+                <h3>Edit Note</h3>
+                    <form onSubmit = { this.onSubmit }>
+                        <div className = "form-group">
+                            <label>Username: </label>
+                            <select ref = "userInput"
+                                required
+                                className = "form-control"
+                                value = { this.state.username }
+                                onChange={ this.onChangeUsername }>
+                                    {
+                                        this.state.users.map(function(user){
+                                            return <option
+                                            key = { user }
+                                        value = { user }>{ user }
+                                        </option>;
+
+                                      })
+                                    }
+                                </select>
+                        </div>
+                        <div className="form-group">
+                            <label>Title: </label>
+                            <input type = "text"
+                                required
+                                className = "form-control"
+                                value = { this.state.title }
+                                onChange = { this.onChangeTitle }
+                                ></input>
+                        </div>
+                        <div className="form-group">
+                            <label>Content: </label>
+                            <input type = "text"
+                                required
+                                className = "form-control"
+                                value = { this.state.content }
+                                onChange = { this.onChangeContent }
+                                ></input>
+                        </div>
+
+                        <div className = "form-group">
+                            <input type = "submit" value = "Edit Note" className = "btn btn-primary"></input>
+                        </div>
+                    </form>
             </div>
         )
     }
--- src/components/notes-list.component.js
+++ src/components/notes-list.component.js
@@ -1,10 +1,71 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Note = props => (
+    <tr>
+        <td>{ props.note.username }</td>
+        <td>{ props.note.title } </td>
+        <td>{ props.note.content }</td>
+        <td>
+            <Link to = {"/edit/" + props.note._id}>Edit</Link> | <a href="#" onClick={() => { props.deleteNote(props.note._id) }}>Delete</a>
+        </td>
+    </tr>
+)
 
 export default class NoteList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.deleteNote = this.deleteNote.bind(this);
+
+        this.state = {notes: []};
+
+    }   
+    
+    componentDidMount(){
+        axios.get('http://localhost:5000/notes/')
+            .then(response => {
+                this.setState({ notes: response.data });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    deleteNote(id) {
+        axios.delete('http://localhost:5000/notes/' + id)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            notes: this.state.notes.filter(el => el.id !== id)
+        })
+        window.location = '/';
+    }
+    
+    NoteList() {
+        return this.state.notes.map(currentNote => {
+            return <Note note = { currentNote } deleteNote = { this.deleteNote } key = { currentNote._id }></Note>
+        })
+    }
+
     render() {
         return (
             <div>
-                <p>YOU ARE ON THE NOTES LIST Component</p>
+                <h3>Notes</h3>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Username</th>
+                            <th>Title</th>
+                            <th>Content</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.NoteList() }
+                    </tbody>
+                </table>
             </div>
         )
     }
